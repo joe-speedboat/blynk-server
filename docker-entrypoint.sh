54d51a8c1e1540dd0ab21038a11ea9980ecf0c14
Chris Ruettimann <chris@bitbull.ch>
2018-12-31 11:34:41 +0100

    bugfix, removed /config/

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 test -d /data/config || mkdir -p /data/config
-test -f /config/server.properties || touch /config/server.properties
+test -f /data/config/server.properties || touch /data/config/server.properties
 
 if [ "$1" = 'debug' ]; then
     exec java -verbose:class -jar /blynk/server.jar -dataFolder /data -serverConfig /config/server.properties
